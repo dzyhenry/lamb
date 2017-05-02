92e1e1e1e838b5ef22e064f12decb1a47469e990
dzyhenry <dzyhenry@foxmail.com>
2017-05-02 17:16:47 +0800

    write some test code

---
+++ test/test.js
@@ -0,0 +1,30 @@
+const lamb = require('../lamb.js');
+
+const Promise = lamb.Lamb;
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(new Promise((resov, rej) => {
+      setTimeout(() => {
+        console.log('In promise 1, we return a new Promise: ');
+        resov(123);
+      }, 100);
+    }));
+  }, 100);
+});
+
+promise.then((data) => {
+  console.log('first then: ', data);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(data + 12);
+    }, 100);
+  });
+}, (reason) => {
+  console.log('first reject reason: ', reason);
+  return reason + 123;
+}).then((data) => {
+  console.log('second then: ',data);
+}, (reason) => {
+  console.log('second reject reason: ', reason);
+});
